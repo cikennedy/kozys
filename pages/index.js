8bae40cc3c303b2b5e917fc8c5d4c49e9b7e377d
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-20 10:32:53 -0500

    styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <div>
+        <h2>Best Sellers</h2>
+        <p>Bicycles and more!</p>
+      </div>
+      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      Footer
+    </>
+  );
 };
 
-export default index;
+export default Home;
