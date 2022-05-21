97a5dbae496d073b6d1c63c4efab30902afc98d9
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-21 13:44:43 -0500

    footer updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 Kozy's Bicycles All Rights Reserved</p>
+    </div>
   )
 }
 
