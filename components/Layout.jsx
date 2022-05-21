1faafcebc30940e5e555d9355c4adea1de6d0328
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-21 13:33:27 -0500

    layout component and rendering children

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
         <title>Kozy's</title>
       </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
