49f519fb478cf866a527e1ede336282aa3337f11
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-21 13:42:49 -0500

    styling changes to banners

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image } }) => {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image, desc } }) => {
   return (
     <div className='footer-banner-container'>
       <div className='banner-desc'>
