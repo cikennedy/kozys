fc5fac703fa5927073282431a2759988c3024cb9
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-22 10:05:21 -0500

    dynamic product details component, need to make api call to fetch specific product data

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,20 @@
 // square bracket named file means that this page will be dynamically rendered
 import React from "react";
 
+import { client, urlFor } from "../../lib/client";
+
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
