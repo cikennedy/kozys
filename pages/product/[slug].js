3e3979bb69ccc2ad59cf787be6c76c213ebe3e89
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-22 10:19:51 -0500

    getStaticPaths added to for dynamic routes for each product, necessary to be able to render pages

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,18 +3,43 @@ import React from "react";
 
 import { client, urlFor } from "../../lib/client";
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src="" />
+            <img src={urlFor(image && image[0])} />
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+// if using getStaticProps, need to use getStaticPaths via next.js documentation
+// https://nextjs.org/docs/basic-features/data-fetching/get-static-paths
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }
+    `;
+
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
 };
 
 // getStaticProps is a next.js function that will pre-render a page at build time
