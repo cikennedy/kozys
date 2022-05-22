e2ce5251a788745fb3134c8db8e8bf555a300b23
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-22 10:12:35 -0500

    getStaticProps next.js function created to pre-render product page at build time

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -17,4 +17,18 @@ const ProductDetails = () => {
   );
 };
 
+// getStaticProps is a next.js function that will pre-render a page at build time
+// use if data comes from a headless CMS, can be publicly cahced (not user-specific)
+// getStaticProps generates HTML and JSON files, both can be cached by a CDN for performance
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { products, product },
+  };
+};
+
 export default ProductDetails;
