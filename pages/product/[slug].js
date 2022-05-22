e98058de2d33f09ec4eaf10bc871d80108bbf39a
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-22 10:00:28 -0500

    template rafce product details page

---
+++ pages/product/[slug].js
@@ -0,0 +1,8 @@
+// square bracket named file means that this page will be dynamically rendered
+import React from "react";
+
+const ProductDetails = () => {
+  return <div>ProductDetails</div>;
+};
+
+export default ProductDetails;
