3952c274f9319c98a8e4d2e86325f0da4ae5894c
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-23 10:26:00 -0500

    only show cart if clicked

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <Link href='/'>Kozy's</Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick=''>
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
         <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
