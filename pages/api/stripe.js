11ba1e66b05b57a1787d85dc8564ddad1c72737e
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-26 18:16:43 -0500

    checkout with stripe functionality

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -39,13 +39,12 @@ export default async function handler(req, res) {
             quantity: item.quantity,
           };
         }),
-        mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
