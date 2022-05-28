c4cc447b6b445eaec702108744849a7bf6466da3
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-27 19:15:20 -0500

    useStateContext imports, use state snippets

--- pages/success.js
+++ pages/success.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import Link from 'next/link';
-import { BsBagCheckFill } from 'react-icons/bs'
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 
-
+import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  const [order, setOrder] = useState();
   return <div>success</div>;
 };
 
